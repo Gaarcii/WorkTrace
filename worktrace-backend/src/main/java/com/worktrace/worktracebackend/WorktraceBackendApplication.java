4a0f412e294c75fd262062dff5b5441a861ee8aa
David_Garcia_Torres <david.garca4@educa.madrid.org>
2026-03-12 22:12:55 +0100

    Creado y configurado el proyecto de springboot

---
+++ worktrace-backend/src/main/java/com/worktrace/worktracebackend/WorktraceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.worktrace.worktracebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktraceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorktraceBackendApplication.class, args);
+    }
+
+}
---
+++ worktrace-backend/src/test/java/com/worktrace/worktracebackend/WorktraceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worktrace.worktracebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktraceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
