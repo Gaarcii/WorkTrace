9a3d911657e69fcb203aa895ca1c9f8a38e8cdd9
David_Garcia_Torres <david.garca4@educa.madrid.org>
2026-03-13 01:11:57 +0100

    .

--- database/schema.sql
+++ database/schema.sql
@@ -6,9 +6,7 @@ create table if not exists companies (
     company_name text not null,
     cif text null,
     logo_url text null,
-    updated_at timestamp
-    with
-        time zone not null default now()
+    updated_at timestamp with time zone not null default now()
 );
 
 -- Autentificación
@@ -18,55 +16,45 @@ create table if not exists users (
     password_hash text not null,
     role text not null,
     is_enabled boolean not null default true,
-    created_at timestamp
-    with
-        time zone not null default now(),
-        company_id uuid not null,
-        check (
-            role in (
-                'WORKER',
-                'ADMIN',
-                'INSPECTOR'
-            )
+    created_at timestamp with time zone not null default now(),
+    company_id uuid not null,
+    check (
+        role in (
+            'WORKER',
+            'ADMIN',
+            'INSPECTOR'
         )
+    )
 );
 
-create table if not exists profiles
-(
-    user_id        uuid primary key,
-    full_name      text                     not null,
-    employee_code  text                     not null,
-    is_active      boolean                  not null default true,
-    preferences    jsonb                    null     default '{}'::jsonb,
-    phone          text                     null,
-    avatar_url     text                     null,
-    is_first_login boolean                  not null default true,
-    updated_at     timestamp with time zone not null default now(),
-    weekly_hours   numeric                  null,
-    position_id    uuid                     null
-    );
+create table if not exists profiles (
+    user_id uuid primary key,
+    full_name text not null,
+    employee_code text not null,
+    is_active boolean not null default true,
+    phone text null,
+    avatar_url text null,
+    is_first_login boolean not null default true,
+    updated_at timestamp with time zone not null default now(),
+    weekly_hours numeric null,
+    position_id uuid null
+);
 
 create table if not exists job_positions (
     id uuid primary key default gen_random_uuid (),
     title text not null,
-    created_at timestamp
-    with
-        time zone not null default now(),
-        company_id uuid not null,
-        unique (title, company_id) -- Único por empresa
+    created_at timestamp with time zone not null default now(),
+    company_id uuid not null,
+    unique (title, company_id) -- Único por empresa
 );
 
 create table if not exists work_sites (
     id uuid primary key default gen_random_uuid (),
     name text not null,
     address text not null,
-    created_at timestamp
-    with
-        time zone not null default now(),
-        updated_at timestamp
-    with
-        time zone not null default now(),
-        company_id uuid not null
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone not null default now(),
+    company_id uuid not null
 );
 
 create table if not exists work_schedules (
@@ -76,88 +64,63 @@ create table if not exists work_schedules (
     day_of_week integer not null,
     start_time time without time zone not null,
     end_time time without time zone not null,
-    created_at timestamp
-    with
-        time zone not null default now(),
-        updated_at timestamp
-    with
-        time zone not null default now(),
-        company_id uuid not null,
-        check (day_of_week between 0 and 6)
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone not null default now(),
+    company_id uuid not null,
+    check (day_of_week between 0 and 6)
 );
 
 --Sistema de fichajes
-create table if not exists time_entries
-(
-    id                  uuid primary key         not null default gen_random_uuid(),
-    employee_id         uuid                     not null,
-    work_date           date                     not null,
-    start_at            timestamp with time zone not null,
-    end_at              timestamp with time zone null,
-                                      start_lat numeric (9, 6) not null,
-    start_lng           numeric(9, 6)            not null,
-    end_lat             numeric(9, 6)            null,
-    end_lng             numeric(9, 6)            null,
-    start_accuracy_m    integer                  not null,
-    end_accuracy_m      integer                  null,
-    start_ip            inet                     not null,
-    end_ip              inet                     null,
-    start_user_agent    text                     not null,
-    end_user_agent      text                     null,
-    start_geoip         jsonb                    null,
-    end_geoip           jsonb                    null,
-    flags               text[]                   not null default '{}'::text[],
-    status              text                     not null,
-    deleted_at          timestamp with time zone null,
-    deleted_by          uuid                     null,
-    delete_reason       text                     null,
-    created_at          timestamp with time zone not null default now(),
-    created_by          uuid                     not null,
-    modification_reason text                     null,
+create table if not exists time_entries (
+    id uuid primary key not null default gen_random_uuid (),
+    employee_id uuid not null,
+    work_date date not null,
+    start_at timestamp with time zone not null,
+    end_at timestamp with time zone null,
+    start_lat numeric(9, 6) not null,
+    start_lng numeric(9, 6) not null,
+    end_lat numeric(9, 6) null,
+    end_lng numeric(9, 6) null,
+    start_accuracy_m integer not null,
+    end_accuracy_m integer null,
+    start_ip inet not null,
+    end_ip inet null,
+    start_user_agent text not null,
+    end_user_agent text null,
+    start_geoip jsonb null,
+    end_geoip jsonb null,
+    flags text [] not null default '{}'::text [],
+    status text not null,
+    deleted_at timestamp with time zone null,
+    deleted_by uuid null,
+    delete_reason text null,
+    created_at timestamp with time zone not null default now(),
+    created_by uuid not null,
+    modification_reason text null,
     company_id uuid not null,
     check (status in ('OPEN', 'CLOSED')),
     check (
-(
-              deleted_at
-              is
-              null
-              and
-              deleted_by
-              is
-              null
-              and
-              delete_reason
-              is
-              null
-)
-    or
-(
-    deleted_at
-    is
-    not
-    null
-    and
-    deleted_by
-    is
-    not
-    null
-    and
-    delete_reason
-    is
-    not
-    null
-)
+        (
+            deleted_at is null
+            and deleted_by is null
+            and delete_reason is null
+        )
+        or (
+            deleted_at is not null
+            and deleted_by is not null
+            and delete_reason is not null
+        )
     )
-    );
+);
 
+known_ips
 create table if not exists daily_closures (
     work_date date primary key not null,
     records_count integer not null,
     day_hash text not null,
     prev_day_hash text null,
-    computed_at timestamp
-    with
-        time zone not null default now()
+    company_id uuid not null,
+    computed_at timestamp with time zone not null default now()
 );
 
 create table if not exists audit_time_entries (
@@ -168,49 +131,49 @@ create table if not exists audit_time_entries (
     reason text not null,
     old_data jsonb,
     new_data jsonb,
-    created_at timestamp
-    with
-        time zone not null default now(),
-        company_id uuid not null, -- Faltaba esto
-        check (
-            action in ('ADMIN_ADJUST', 'SOFT_DELETE')
-        )
+    created_at timestamp with time zone not null default now(),
+    company_id uuid not null, -- Faltaba esto
+    check (
+        action in ('ADMIN_ADJUST', 'SOFT_DELETE')
+    )
 );
 
 --Incidencias y antifraude
-create table if not exists incidents
-(
-    id             uuid primary key         not null default gen_random_uuid(),
-    user_id        uuid                     not null,
-    date           date                     not null,
-    comment        text                     not null,
+create table if not exists incidents (
+    id uuid primary key not null default gen_random_uuid (),
+    user_id uuid not null,
+    date date not null,
+    comment text not null,
     status text not null default 'PENDING'::text,
-    admin_response text                     null,
-    created_at     timestamp with time zone not null default now(),
-    updated_at     timestamp with time zone not null default now(),
-    resolved_by    uuid                     null,
-    type_id        uuid                     not null,
-    incident_time  time without time zone   not null,
+    admin_response text null,
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone not null default now(),
+    resolved_by uuid null,
+    type_id uuid not null,
+    incident_time time without time zone not null,
     company_id uuid not null,
-    check (status in ('PENDING', 'RESOLVED', 'REJECTED'))
-    );
+    check (
+        status in (
+            'PENDING',
+            'RESOLVED',
+            'REJECTED'
+        )
+    )
+);
 
 create table if not exists incidence_types (
     id uuid primary key default gen_random_uuid (),
     name text not null,
-    created_at timestamp
-    with
-        time zone not null default now(),
-        company_id uuid not null,
-        unique (name, company_id)
+    created_at timestamp with time zone not null default now(),
+    company_id uuid not null,
+    unique (name, company_id)
 );
 
 create table if not exists known_ips (
     ip inet primary key,
     geoip_data jsonb not null,
-    created_at timestamp
-    with
-        time zone not null default now()
+    company_id uuid not null,
+    created_at timestamp with time zone not null default now()
 );
 
 alter table users
@@ -236,6 +199,12 @@ add constraint fk_incidents_company foreign key (company_id) references companie
 
 alter table incidence_types
 add constraint fk_inc_types_company foreign key (company_id) references companies (id);
+
+alter table daily_closures
+add constraint fk_closures_company foreign key (company_id) references companies (id);
+
+alter table known_ips
+add constraint fk_known_ips_company foreign key (company_id) references companies (id);
 
 alter table profiles
 add constraint fk_profiles_user foreign key (user_id) references users (id) on delete cascade;
