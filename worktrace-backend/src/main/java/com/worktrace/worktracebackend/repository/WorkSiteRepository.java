a4e6f91ce07b3c7b5d2840ceb9d352df6df5dbd3
David_Garcia_Torres <david.garca4@educa.madrid.org>
2026-03-29 12:59:44 +0200

    fix

--- worktrace-backend/src/main/java/com/worktrace/worktracebackend/repository/WorkSiteRepository.java
+++ worktrace-backend/src/main/java/com/worktrace/worktracebackend/repository/WorkSiteRepository.java
@@ -4,11 +4,8 @@ import com.worktrace.worktracebackend.model.WorkSite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WorkSiteRepository extends JpaRepository<WorkSite, UUID> {
-
-    Optional<WorkSite> findWorkSiteById(UUID id);
 }
