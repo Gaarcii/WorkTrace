22dc1d1477b86a08b64ecfff4bea3f3f03aa9fe1
David_Garcia_Torres <david.garca4@educa.madrid.org>
2026-05-12 15:57:12 +0200

    .

--- worktrace-frontend/src/app/core/api/api.config.ts
+++ worktrace-frontend/src/app/core/api/api.config.ts
@@ -1,5 +1,3 @@
 export const API_CONFIG = {
-    // Usar ruta relativa permite que Nginx (en producción) reenvíe la llamada
-    // al backend sin exponer orígenes distintos, evitando así problemas de CORS.
-    baseUrl: '/api'
+    baseUrl:"http://localhost:8080/api/"
 } as const;
